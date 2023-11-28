c86733a832230a6a580fd5d8194195d5fa1ea280
Tom Bates <thomas1602@hotmail.co.uk>
2023-11-28 19:52:35 +0000

    creates to appends to csv on server

--- server.js
+++ server.js
@@ -2,10 +2,34 @@ console.log("SocketServer running");
 
 // start with express
 var express = require('express'); // these variables are function calls
+const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
 var app = express();
 var server = app.listen(3000);
 
 app.use(express.static('public')); 
+app.use(bodyParser.json());
+
+app.post('/saveWorkoutData', async (req, res) => {
+    const { filename, data } = req.body;
+    console.log("/saveWorkoutData POST call received.")
+    console.log(`filename: '${filename}'`)
+    try {
+        // Check if the file exists
+        await fs.access(`public/stats/${filename}`);
+        
+        // File exists, append data
+        await fs.appendFile(`public/stats/${filename}`, `${data}\n`);
+        res.status(200).send('Data appended to existing file.');
+    } catch {
+        // File doesn't exist, create a new file
+        console.log(`File '${filename}' doesn't exist, creating...`)
+        await fs.writeFile(`public/stats/${filename}`, "Date,ExerciseId,NumberOfSets\n");
+        await fs.appendFile(`public/stats/${filename}`, `${data}\n`);
+        res.status(201).send('New file created.');
+    }
+});
 
 // serve favicon
 // var favicon = require('serve-favicon');
@@ -20,3 +44,4 @@ function newConnection(socket)
 {
     console.log('new connection: ' + socket.id);
 }
+
